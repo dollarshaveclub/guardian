fc7d9cc16a4e60a9a099177d70bdeaaf380db536
Hank Jacobs <hank8903@gmail.com>
2018-04-09 14:55:39 -0700

    Properly format authority tag for datadog metrics

--- pkg/guardian/metrics.go
+++ pkg/guardian/metrics.go
@@ -17,10 +17,12 @@ type DataDogReporter struct {
 
 const durationMetricName = "request.duration"
 const blockedKey = "blocked"
+const authorityKey = "authority"
 
 func (d *DataDogReporter) Duration(request Request, blocked bool, duration time.Duration) error {
+	authorityTag := fmt.Sprintf("%v: %v", authorityKey, request.Authority)
 	blockedTag := fmt.Sprintf("%v: %v", blockedKey, blocked)
-	return d.Client.TimeInMilliseconds(durationMetricName, float64(duration/time.Millisecond), []string{request.Authority, blockedTag}, 1)
+	return d.Client.TimeInMilliseconds(durationMetricName, float64(duration/time.Millisecond), []string{authorityTag, blockedTag}, 1)
 }
 
 type NullReporter struct{}
