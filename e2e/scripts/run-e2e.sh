7c943f070ffef15fbe8af07b834cef19789c9608
Benjamen Keroack <benjamen@dollarshaveclub.com>
2019-04-08 15:54:25 -0700

    fix e2e script, Go 1.12

--- e2e/scripts/run-e2e.sh
+++ e2e/scripts/run-e2e.sh
@@ -2,10 +2,16 @@
 
 echo "Starting async guardian..."
 docker-compose -f e2e/docker-compose-async.yml up -d --build --force-recreate > /dev/null 2>&1
-sleep 30 # wait for guardian to finish starting up
+[[ $? -gt 0 ]] && exit 1
+secs=30 # wait for guardian to finish starting up
+while [ $secs -ge 0 ]; do
+   echo -ne "Waiting for guardian to start up...$secs\033[0K\r"
+   sleep 1
+   : $((secs--))
+done
 
-echo "Running..."
-GOCACHE=off go test ./e2e/
+printf "\nRunning tests...\n"
+go test ./e2e/
 results=$?
 
 echo "Stopping async guardian..."
@@ -15,10 +21,16 @@ if [ "$results" != 0 ] ; then exit 1; fi
 
 echo "Starting sync guardian..."
 docker-compose -f e2e/docker-compose-sync.yml up -d --build --force-recreate > /dev/null 2>&1
-sleep 30 # wait for guardian to finish starting up
+[[ $? -gt 0 ]] && exit 1
+secs=30 # wait for guardian to finish starting up
+while [ $secs -ge 0 ]; do
+   echo -ne "Waiting for guardian to start up...$secs\033[0K\r"
+   sleep 1
+   : $((secs--))
+done
 
-echo "Running..."
-GOCACHE=off SYNC=true go test ./e2e/
+printf "\nRunning tests...\n"
+SYNC=true go test ./e2e/
 results=$?
 
 echo "Stopping sync guardian..."
