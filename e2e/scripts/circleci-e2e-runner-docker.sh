5232b84ca2813004a71b0173ada8efbbbfe968c6
William Alexander <wwalexander@users.noreply.github.com>
2020-06-01 19:34:55 -0700

    Avoid running E2E tests in parallel

--- e2e/scripts/circleci-e2e-runner-docker.sh
+++ e2e/scripts/circleci-e2e-runner-docker.sh
@@ -3,4 +3,4 @@
 cd /go/src/github.com/dollarshaveclub/guardian
 
 COMMIT="e2e" make cli
-go test ./e2e -redis-addr="redis:6379" -envoy-addr="envoy:8080"
+go test ./e2e -p 1 -redis-addr="redis:6379" -envoy-addr="envoy:8080"
